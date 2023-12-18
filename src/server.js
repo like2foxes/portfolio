8409504e6b14a551e65aa6b6e8d93411ffd9a940
Shai Cioara <shaicioara@gmail.com>
2023-12-18 07:29:47 +0000

    added the about route

--- src/server.js
+++ src/server.js
@@ -1,2 +1,60 @@
 'use strict';
-console.log('Hello World');
+
+import http from 'http';
+import fs from 'fs';
+
+const server = http.createServer(router);
+
+server.listen(3000, () => {
+	console.log('Server is listening on port 3000');
+});
+
+export { server };
+
+/**
+	* @param {http.IncomingMessage} req - An http request object
+	* @param {http.ServerResponse} res - An http response object
+	*/
+function router(req, res) {
+	if (!req.url || !req.method) {
+		res.statusCode = 500;
+		res.end('E_SERVER_ERROR');
+		return;
+	}
+	if (req.method === 'GET') {
+		if (req.url === '/') {
+			fs.readFile('./src/routes/index.html', (err, data) => {
+				if (err) {
+					console.error(err);
+					res.statusCode = 500;
+					res.end('E_SERVER_ERROR');
+				}
+				else {
+					res.statusCode = 200;
+					res.setHeader('Content-Type', 'text/html');
+					res.end(data);
+				}
+			})
+			return;
+		}
+		if(req.url === '/about') {
+			fs.readFile('./src/routes/about/index.html', (err, data) => {
+				if (err) {
+					console.error(err);
+					res.statusCode = 500;
+					res.end('E_SERVER_ERROR');
+				}
+				else {
+					res.statusCode = 200;
+					res.setHeader('Content-Type', 'text/html');
+					res.end(data);
+				}
+			})
+			return;
+		}
+
+		res.statusCode = 404;
+		res.end('E_NOT_FOUND');
+	}
+
+}
